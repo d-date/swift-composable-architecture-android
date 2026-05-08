a3f216473956614615a3a6364793c07cbf2f5139
Daiki Matsudate <20222809+d-date@users.noreply.github.com>
2026-05-08 12:11:17 +0900

    Revert "Pin combine-schedulers identity name when redirecting to Android fork"
    This reverts commit 532b22a6f13a8191d1c122cdc1ce55612ebfbe97.

--- Package.swift
+++ Package.swift
@@ -41,7 +41,6 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-collections", from: "1.1.0"),
     .package(
-      name: "combine-schedulers",
       url: "git@github.com:d-date/combine-schedulers-android.git",
       branch: "main-android"
     ),
